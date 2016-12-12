378ecc2abd6a51e531113f54f104278ddb37663f
MEM-mosaic <reginaa96@gmail.com>
2016-12-13 01:44:53 +0300

    Revert "Бд и картинки"
    This reverts commit cfae7d2a11d6b4107d2ad2401e52115c4141dedd.

--- mosaic/Program.cs
+++ mosaic/Program.cs
@@ -4,24 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace mosaic
 {
     class Program
     {
         static void Main(string[] args)
-        {
-           
-            //Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
-            Repository r = new Repository();
-
-            using (var c = new Context())
-            {
-                //  foreach (Links l in c.Links) c.Links.Remove(l);
-                c.Links.AddRange(r.Links);
-                c.SaveChanges();
-            }
-          //  Console.ReadKey();
+        { //ятоппрогер
         }
     }
 }
